0cb5c47e02d8ed154f7b05ffee7239321e43cffa
grv <rafdoro@gmail.com>
2015-12-06 22:56:43 +0100

    Add avg_notes in StudentDecorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.for_subject_item(subject_item).average(:value)
+    notes.nil? ? "0.00" : "%.2f" % notes
   end
 end
